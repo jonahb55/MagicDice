4d14264df6362b93d4e8a9b0ad3c62e737a99e78
Jonah Bonner <jonahwbonner@gmail.com>
2020-12-14 18:45:46 -0500

    Add multithreading

--- magic-dice.py
+++ magic-dice.py
@@ -1,26 +1,6 @@
+import multiprocessing as mp
 import random
-
-total_game_count = int(input("How many games? "))
-print_output = input("Print output? (slower) ") == "y"
-
-total_win_count = 0
-magic_game_count = {
-    4: 0,
-    5: 0,
-    6: 0,
-    8: 0,
-    9: 0,
-    10: 0
-}
-magic_win_count = {
-    4: 0,
-    5: 0,
-    6: 0,
-    8: 0,
-    9: 0,
-    10: 0
-}
-roll_count_distribution = [0] * 999
+import math
 
 
 # Roll two dice and get the sum
@@ -28,54 +8,129 @@ def roll():
     return random.randint(1, 6) + random.randint(1, 6)
 
 
-for i in range(total_game_count):
-    # Play the game!
-    rolls = [roll()]
-    if rolls[0] == 7 or rolls[0] == 11:
-        win = True
-    elif rolls[0] == 2 or rolls[0] == 3 or rolls[0] == 12:
-        win = False
-    else:
-        magic_game_count[rolls[0]] += 1
-        rolls.append(roll())
-        while rolls[-1] != 7 and rolls[-1] != rolls[0]:
-            rolls.append(roll())
-        if rolls[-1] == 7:
+# Play a series of games and return the results
+def play_series(game_count, queue):
+    results = {
+        "total_win_count": 0,
+        "magic_game_count": {
+            4: 0,
+            5: 0,
+            6: 0,
+            8: 0,
+            9: 0,
+            10: 0
+        },
+        "magic_win_count": {
+            4: 0,
+            5: 0,
+            6: 0,
+            8: 0,
+            9: 0,
+            10: 0,
+        },
+        "roll_count_distribution": []
+    }
+
+    # Loop through each game
+    for i in range(game_count):
+        # Play the game!
+        rolls = [roll()]
+        if rolls[0] == 7 or rolls[0] == 11:
+            win = True
+        elif rolls[0] == 2 or rolls[0] == 3 or rolls[0] == 12:
             win = False
         else:
-            win = True
-            magic_win_count[rolls[0]] += 1
+            results["magic_game_count"][rolls[0]] += 1
+            rolls.append(roll())
+            while rolls[-1] != 7 and rolls[-1] != rolls[0]:
+                rolls.append(roll())
+            if rolls[-1] == 7:
+                win = False
+            else:
+                win = True
+                results["magic_win_count"][rolls[0]] += 1
 
-    # Record data
-    if win:
-        total_win_count += 1
+        # Record win
+        if win:
+            results["total_win_count"] += 1
 
-    roll_count_distribution[len(rolls)] += 1
+        # Record roll count
+        while len(results["roll_count_distribution"]) <= len(rolls):
+            results["roll_count_distribution"].append(0)
+        results["roll_count_distribution"][len(rolls)] += 1
 
-    # Print log of gameplay
-    if print_output:
-        print(",".join([str(x) for x in rolls]) +
-              " - " + ("Win" if win else "Loss"))
+    # Return final results
+    queue.put(results)
 
-# Print results
-print("-----------------")
 
-print(str(total_game_count) + " total games")
-print(str(total_win_count) + " total wins")
-print()
-print(str(sum(magic_game_count.values())) + " \"magic\" games")
-print(str(sum(magic_win_count.values())) + " \"magic\" wins")
+# Main code
+if __name__ == "__main__":
+    total_game_count = int(input("How many games? "))
 
-print("\nMagic win distribution:")
-for i in [4, 5, 6, 8, 9, 10]:
-    print("Roll " + str(i).zfill(2) + " - " +
-          str(magic_win_count[i]) + "/" + str(magic_game_count[i]))
+    # Divide games between threads
+    core_count = mp.cpu_count()
+    thread_count = core_count * 4
+    print("Using " + str(thread_count) +
+          " threads (" + str(core_count) + " cores detected)")
+    thread_game_counts = [
+        math.floor(total_game_count / thread_count)] * thread_count
+    remaining_game_count = total_game_count - sum(thread_game_counts)
+    for i in range(remaining_game_count):
+        thread_game_counts[i] += 1
 
-print("\nRoll count distribution:")
-for i in list(range(len(roll_count_distribution)))[::-1]:
-    if roll_count_distribution[i] > 0:
-        longest_roll = i
-        break
-for i in range(longest_roll + 1):
-    print(str(i).zfill(2) + " rolls - " +
-          str(roll_count_distribution[i]))
+    # Start threads
+    queue = mp.Queue()  # Queue for returning results from each thread
+    for i in range(thread_count):
+        process = mp.Process(target=play_series,
+                             args=(thread_game_counts[i], queue))
+        process.start()
+
+    # Record results
+    total_win_count = 0
+    magic_game_count = {
+        4: 0,
+        5: 0,
+        6: 0,
+        8: 0,
+        9: 0,
+        10: 0
+    }
+    magic_win_count = {
+        4: 0,
+        5: 0,
+        6: 0,
+        8: 0,
+        9: 0,
+        10: 0
+    }
+    roll_count_distribution = []
+    for i in range(thread_count):
+        result = queue.get()
+
+        total_win_count += result["total_win_count"]
+        for magic, count in result["magic_game_count"].items():
+            magic_game_count[magic] += count
+        for magic, count in result["magic_win_count"].items():
+            magic_win_count[magic] += count
+        for f in range(len(result["roll_count_distribution"])):
+            if len(roll_count_distribution) <= f:
+                roll_count_distribution.append(0)
+            roll_count_distribution[f] += result["roll_count_distribution"][f]
+
+    # Print results
+    print("-------------------------")
+
+    print(str(total_game_count) + " total games")
+    print(str(total_win_count) + " total wins")
+    print()
+    print(str(sum(magic_game_count.values())) + " \"magic\" games")
+    print(str(sum(magic_win_count.values())) + " \"magic\" wins")
+
+    print("\nMagic win distribution:")
+    for i in [4, 5, 6, 8, 9, 10]:
+        print("Roll " + str(i).zfill(2) + " - " +
+              str(magic_win_count[i]) + "/" + str(magic_game_count[i]))
+
+    print("\nRoll count distribution:")
+    for i in range(1, len(roll_count_distribution)):
+        print(str(i).zfill(2) + " rolls - " + str(roll_count_distribution[i]))
